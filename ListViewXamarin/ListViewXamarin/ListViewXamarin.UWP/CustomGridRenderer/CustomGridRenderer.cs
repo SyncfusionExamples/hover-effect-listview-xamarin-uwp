9b87c79d653c0260ef27c141273c4935f81e0843
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-09-17 18:01:09 +0530

    XAMARIN-36133 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin.UWP/CustomGridRenderer/CustomGridRenderer.cs
@@ -0,0 +1,54 @@
+﻿using ListViewXamarin;
+using ListViewXamarin.UWP;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomGrid), typeof(CustomGridRenderer))]
+namespace ListViewXamarin.UWP
+{
+    #region CustomGridRenderer
+    public class CustomGridRenderer : VisualElementRenderer<CustomGrid, FrameworkElement>
+    {
+        #region Constructor
+        public CustomGridRenderer()
+        {
+            this.PointerEntered += CustomGridRenderer_PointerEntered;
+            this.PointerExited += CustomGridRenderer_PointerExited;
+            this.PointerPressed += CustomGridRenderer_PointerPressed;
+        }
+        #endregion
+
+        #region Callbacks
+        private void CustomGridRenderer_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            var item = this.Element;
+            item.BackgroundColor = Xamarin.Forms.Color.FromHex("#a6dcef");
+        }
+
+        private void CustomGridRenderer_PointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            var item = this.Element;
+            item.BackgroundColor = Xamarin.Forms.Color.Transparent;
+        }
+
+        private void CustomGridRenderer_PointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            var item = this.Element;
+            var itemData = item.BindingContext as Contacts;
+            if (itemData.IsSelected)
+                item.BackgroundColor = Xamarin.Forms.Color.Transparent;
+            else
+                item.BackgroundColor = Xamarin.Forms.Color.WhiteSmoke;
+        }
+        #endregion
+    }
+    #endregion
+}
+
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/CustomGrid.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class CustomGrid : Grid
+    {
+        
+    }
+}
